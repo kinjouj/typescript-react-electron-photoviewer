4a62a7eb41b0d0b1fc4910a28def6632e3997217
kinjouj <kinjouj@gmail.com>
2025-11-14 15:59:21 +0900

    update

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,24 @@
-import { PhotoView } from './components';
+import { useFetchFiles } from './hooks';
+import { NowLoading, SliderContent } from './components';
 
 const App = (): React.JSX.Element => {
-  return (<PhotoView />);
+  const { files, loading, isError } = useFetchFiles();
+
+  if (loading) {
+    return (<NowLoading loading={loading} />);
+  }
+
+  if (isError) {
+    return (<div>Error</div>);
+  }
+
+  if (files.length === 0) {
+    return (<div>File is Missing</div>);
+  }
+
+  return (
+    <SliderContent files={files} />
+  );
 };
 
 export default App;
--- src/renderer/components/index.ts
+++ src/renderer/components/index.ts
@@ -3,6 +3,5 @@ export { default as NextArrow } from './NextArrow';
 export { default as PrevArrow } from './PrevArrow';
 export { default as NowLoading } from './NowLoading';
 export { default as SliderContent } from './SliderContent';
-export { default as PhotoView } from './PhotoView';
 export { default as PhotoViewSlider } from './PhotoViewSlider';
 export { default as PlayToggleButton } from './PlayToggleButton';
